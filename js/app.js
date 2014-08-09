8d60c61292ab01264dcc9bf87b07fe071f3100bc
Rich Wareham <rjw57@cam.ac.uk>
2014-08-09 15:39:31 +0000

    Update documentation

--- js/app.js
+++ js/app.js
@@ -7,8 +7,6 @@
 var LINK_COLOUR = tinycolor({ h: 240, s: 100, v: 75 }).toHexString(),
     SRC_SINK_COLOUR = tinycolor({ h: 0, s: 100, v: 75 }).toHexString();
 
-var DATA_SERVER = '//trafficdata-realtimetraffic.rhcloud.com/data/';
-
 // Create a promise which gets resolved with an object with the following form:
 //  {
 //    graph: <DirectedGraph>, // full resolution traffic network
@@ -24,7 +22,7 @@ var DATA_SERVER = '//trafficdata-realtimetraffic.rhcloud.com/data/';
 //      occupancies: <Object>?,   // map link ids -> occupancy
 //    },
 //  }
-var fetchData = createFetchDataPromise();
+var fetchData = RealtimeTrafficData.createFetchDataPromise();
 
 fetchData.then(function(v) {
   console.log('fetched data');
@@ -35,9 +33,6 @@ fetchData.catch(function(err) {
   console.log('Error fetching data');
   console.error(err);
 });
-
-// Uncomment for testing
-// DATA_SERVER = 'http://localhost:5000/data/';
 
 $(document).ready(function() {
   function stopLoading() { $('body').removeClass('loading'); }
@@ -67,16 +62,13 @@ $(document).ready(function() {
   fetchData.then(function(data) {
     var cache = {};
 
+    var imageElement = document.createElement('img');
+    imageElement.src = 'img/car.png';
+
     map.on('postcompose', function(event) {
       var vectorContext = event.vectorContext, frameState = event.frameState,
           extent = frameState.extent, res = map.getView().getResolution(), tree, graph,
-          spacing = 20*res;
-
-      var imageStyle = new ol.style.Circle({
-          radius: 5, snapToPixel: false,
-          fill: new ol.style.Fill({color: 'yellow'}),
-          stroke: new ol.style.Stroke({color: 'red', width: 1})
-      });
+          carLength = 30, spacing = 2*carLength*res;
 
       // Do we have this extent cached so we don't need to do a spatial search?
       if(!cache.extent ||
@@ -96,7 +88,7 @@ $(document).ready(function() {
         cache.visibleLinks = cache.tree.search(frameState.extent);
       }
 
-      var pointCoords = [], lineStrings = [];
+      var pointCoords = [], lineString = [], colour;
 
       cache.visibleLinks.forEach(function(link) {
         var edge = cache.graph.getEdgeById(link[4]),
@@ -105,37 +97,52 @@ $(document).ready(function() {
             delta = [p2[0]-p1[0], p2[1]-p1[1]],
             deltaLen = Math.sqrt(delta[0]*delta[0] + delta[1]*delta[1]),
             unitDelta = [delta[0]/deltaLen, delta[1]/deltaLen],
+            rotation = Math.atan2(unitDelta[1], unitDelta[0]),
             lambda, offset;
 
         var speed = data.data.speeds[edge.data.id],
             occupancy = data.data.occupancies[edge.data.id];
 
         // Do we have data for this link?
-        if((speed === undefined) || (occupancy === undefined)) {
-          // no, use line
-          lineStrings.push([ p1, p2 ]);
-        } else {
-          offset = (speed / 100) * frameState.time / 1000;
-          offset -= Math.floor(offset);
-          for(lambda = offset*spacing; lambda < deltaLen; lambda += spacing) {
+        colour = (speed === undefined) ? [128, 128, 128, 1] : speed2color(speed.value);
+        vectorContext.setFillStrokeStyle(
+          new ol.style.Fill(),
+          new ol.style.Stroke({
+            color: colour,
+            width: 3,
+          })
+        );
+
+        // draw line
+        lineString = [ p1, p2 ];
+        vectorContext.drawLineStringGeometry(new ol.geom.LineString(lineString), null);
+
+        // Do we have the information for car icons?
+        if((speed !== undefined) && (occupancy !== undefined)) {
+          spacing = Math.min(10, (100/occupancy.value)) * res * carLength;
+          offset = res * (speed.value / 10) * frameState.time / 1000;
+          offset -= spacing * Math.floor(offset/spacing);
+
+          pointCoords = [];
+          for(lambda = offset; lambda < deltaLen; lambda += spacing) {
             pointCoords.push([ p1[0] + unitDelta[0]*lambda, p1[1] + unitDelta[1]*lambda, ]);
           }
-          // todo points
+
+          var imageStyle = new ol.style.Icon({
+              anchor: [0.5, 0.5],
+              rotation: - rotation + 0.5 * Math.PI,
+              rotateWithView: true,
+              snapToPixel: false,
+              img: imageElement,
+              scale: carLength / 100,
+              size: [50,100],
+          });
+
+          vectorContext.setImageStyle(imageStyle);
+          vectorContext.drawMultiPointGeometry(
+              new ol.geom.MultiPoint(pointCoords), null);
         }
       });
-
-      vectorContext.setImageStyle(imageStyle);
-      vectorContext.drawMultiPointGeometry(
-          new ol.geom.MultiPoint(pointCoords), null);
-      vectorContext.setFillStrokeStyle(
-        new ol.style.Fill(),
-        new ol.style.Stroke({
-          color: [255,0,0,1],
-          width: 3,
-        })
-      );
-      vectorContext.drawMultiLineStringGeometry(
-        new ol.geom.MultiLineString(lineStrings), null);
 
       map.render();
     });
@@ -143,134 +150,15 @@ $(document).ready(function() {
   });
 });
 
-function createFetchDataPromise() {
-  // Fetch links network
-  var fetchLinks = createFetchLinksPromise();
-
-  // Simplify network
-  var simplify = createSimplifyPromise(fetchLinks);
-
-  // Data promises
-  var dataTypes = ['speeds', 'flows', 'occupancies'],
-      dataPromises = dataTypes.map(createFetchTrafficDataPromise),
-      allDataPromise = Promise.all(dataPromises).then(function(dataSets) {
-        var rv = {};
-        dataSets.forEach(function(ds) {
-          rv[ds.type] = ds.data;
-        });
-        return rv;
-      });
-
-  return Promise.all([fetchLinks, simplify, allDataPromise])
-  .then(function(vs) {
-    return {
-      graph: vs[0],
-      simplified: vs[1],
-      data: vs[2],
-    };
-  });
-}
-
-function createSimplifyPromise(fetchLinks) {
-  return fetchLinks
-  .then(function(G) {
-    // Create networks for various resolutions
-    var maxResolution = 30, minResolution, rv = [];
-
-    while(maxResolution < 800) {
-      rv.push({
-        minResolution: minResolution, maxResolution: maxResolution,
-        graph: G, tree: graphToTree(G),
-      });
-
-      minResolution = maxResolution;
-      maxResolution = maxResolution * 3;
-      G = G.copy().simplify(8 * maxResolution);
-    }
-
-    rv.push({
-      minResolution: minResolution, graph: G, tree: graphToTree(G),
-    });
-
-    return rv;
-  });
-};
-
-function createFetchTrafficDataPromise(type) {
-  var url = DATA_SERVER + type + '.json';
-  return createGetJSONPromise(url)
-  .then(function(data) {
-    var rv = {};
-    data.data.forEach(function(datum) {
-      rv[datum.location] = datum.value;
-    });
-    return { type: type, url: url, data: rv };
-  });
-}
-
-function createFetchLinksPromise(options) {
-  options = _extend({
-    srcProjection: 'EPSG:4326', destProjection: 'EPSG:3857',
-  }, options);
-
-  return createGetJSONPromise(DATA_SERVER + 'network.json')
-  .then(function(network) {
-    // A list of node and edge objects to construct the DirectedGraph
-    var nodes = [], edges = [];
-
-    // Project each of the nodes into the map co-ordinate projection
-    network.graph.nodes.forEach(function(n, nIdx) {
-      if(!n.pos) { return; }
-      n.pos = ol.proj.transform(n.pos, options.srcProjection, options.destProjection);
-      nodes.push({ id: 'Node' + nIdx, data: n });
-    });
-
-    // Now, using the projected nodes, work out the length of each edge.
-    network.graph.links.forEach(function(e, eIdx) {
-      var u = network.graph.nodes[e.source], v = network.graph.nodes[e.target];
-      var dx = v.pos[0] - u.pos[0], dy = v.pos[1] - v.pos[1];
-      e.length = Math.sqrt(dx*dx + dy*dy);
-      edges.push({
-        id: 'Edge' + eIdx,
-        nodes: [ 'Node' + e.source, 'Node' + e.target ],
-        data: e,
-      });
-    });
-
-    // OK, we've fiddled with the network enough to load it into our network class.
-    return new DirectedGraph(nodes, edges);
-  });
-}
-
-function createGetJSONPromise(url) {
-  return new Promise(function(resolve, reject) {
-    $.ajax({
-      dataType: 'json',
-      url: url,
-      success: function(data) { resolve(data); },
-      error: function(jqXHR, textStatus, errorThrown) { reject(errorThrown); },
-    });
-  });
-}
-
 // utility functions
-function graphToTree(graph) {
-  var items = [];
-  graph.getEdges().forEach(function(edge) {
-    var p1 = graph.getNodeById(edge.nodes[0]).data.pos,
-        p2 = graph.getNodeById(edge.nodes[1]).data.pos;
 
-    items.push([
-      Math.min(p1[0], p2[0]), Math.min(p1[1], p2[1]),
-      Math.max(p1[0], p2[0]), Math.max(p1[1], p2[1]),
-      edge.id,
-    ]);
-  });
+function speed2color(speed) {
+  var maxSpeed = 120 /*kmh*/,
+      lambda = Math.max(0, Math.min(1, speed / maxSpeed)),
+      s = Math.sin(lambda*0.5*Math.PI);
 
-  var tree = rbush();
-  tree.load(items);
-  return tree;
-};
+  return [255*(1-s*s), 255*(s*s), 0, 1];
+}
 
 function _extend(obj, otherObj) {
   if(!obj) { return obj; }
--- js/lib.js
+++ js/lib.js
@@ -109412,3 +109412,177 @@ DirectedGraph.prototype.edgesAsGeoJSON = function(nodePosFunc) {
 
 return DirectedGraph;
 })();
+
+// Functions to load data from the network. Provides a single object:
+// RealtimeTrafficData.
+RealtimeTrafficData = (function() {
+'use strict';
+
+var DATA_SERVER = '//trafficdata-realtimetraffic.rhcloud.com/data/';
+
+// Uncomment for testing
+// DATA_SERVER = 'http://localhost:5000/data/';
+
+var RealtimeTrafficData = {};
+
+// Create a promise which gets resolved with an object with the following form:
+//  {
+//    graph: <DirectedGraph>, // full resolution traffic network
+//    simplified: [
+//      {
+//        minResolution: <Number>?, maxResolution: <Number>?,
+//        graph: <DirectedGraph>, tree: <rbush>,
+//      },
+//    ],
+//    data: {
+//      speeds: <Object>?,        // map link ids -> speed
+//      flows: <Object>?,         // map link ids -> flow
+//      occupancies: <Object>?,   // map link ids -> occupancy
+//    },
+//  }
+RealtimeTrafficData.createFetchDataPromise = function() {
+  // Fetch links network
+  var fetchLinks = createFetchLinksPromise();
+
+  // Simplify network
+  var simplify = createSimplifyPromise(fetchLinks);
+
+  // Fetch data and munge into a useful form
+  var fetchData = createGetJSONPromise(DATA_SERVER + 'traffic_data.json')
+  .then(function(data) {
+    var rv = {};
+    ['speeds', 'flows', 'occupancies'].forEach(function(type) {
+      var map = {};
+      data.data[type].forEach(function(datum) {
+        map[datum.location] = { when: new Date(datum.when), value: datum.value };
+      });
+      rv[type] = map;
+    });
+    return { data: rv, metadata: data.metadata, generated: data.generated };
+  });
+
+  return Promise.all([fetchLinks, simplify, fetchData])
+  .then(function(vs) {
+    return {
+      graph: vs[0],
+      simplified: vs[1],
+      data: vs[2].data,
+      timestamps: {
+        data: {
+          published: new Date(vs[2].metadata.publicationtime),
+          generated: new Date(vs[2].generated),
+        },
+      },
+    };
+  });
+}
+
+function createSimplifyPromise(fetchLinks) {
+  return fetchLinks
+  .then(function(G) {
+    // Create networks for various resolutions
+    var maxResolution = 30, minResolution, rv = [];
+
+    while(maxResolution < 800) {
+      rv.push({
+        minResolution: minResolution, maxResolution: maxResolution,
+        graph: G, tree: graphToTree(G),
+      });
+
+      minResolution = maxResolution;
+      maxResolution = maxResolution * 3;
+      G = G.copy().simplify(8 * maxResolution);
+    }
+
+    rv.push({
+      minResolution: minResolution, graph: G, tree: graphToTree(G),
+    });
+
+    return rv;
+  });
+};
+
+function createFetchTrafficDataPromise(type) {
+  var url = DATA_SERVER + type + '.json';
+  return createGetJSONPromise(url)
+  .then(function(data) {
+    var rv = {};
+    data.data.forEach(function(datum) {
+      rv[datum.location] = datum.value;
+    });
+    return { type: type, url: url, data: rv };
+  });
+}
+
+function createFetchLinksPromise(options) {
+  options = _extend({
+    srcProjection: 'EPSG:4326', destProjection: 'EPSG:3857',
+  }, options);
+
+  return createGetJSONPromise(DATA_SERVER + 'network.json')
+  .then(function(network) {
+    // A list of node and edge objects to construct the DirectedGraph
+    var nodes = [], edges = [];
+
+    // Project each of the nodes into the map co-ordinate projection
+    network.graph.nodes.forEach(function(n, nIdx) {
+      if(!n.pos) { return; }
+      n.pos = ol.proj.transform(n.pos, options.srcProjection, options.destProjection);
+      nodes.push({ id: 'Node' + nIdx, data: n });
+    });
+
+    // Now, using the projected nodes, work out the length of each edge.
+    network.graph.links.forEach(function(e, eIdx) {
+      var u = network.graph.nodes[e.source], v = network.graph.nodes[e.target];
+      var dx = v.pos[0] - u.pos[0], dy = v.pos[1] - v.pos[1];
+      e.length = Math.sqrt(dx*dx + dy*dy);
+      edges.push({
+        id: 'Edge' + eIdx,
+        nodes: [ 'Node' + e.source, 'Node' + e.target ],
+        data: e,
+      });
+    });
+
+    // OK, we've fiddled with the network enough to load it into our network class.
+    return new DirectedGraph(nodes, edges);
+  });
+}
+
+function createGetJSONPromise(url) {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      dataType: 'json',
+      url: url,
+      success: function(data) { resolve(data); },
+      error: function(jqXHR, textStatus, errorThrown) { reject(errorThrown); },
+    });
+  });
+}
+
+// utility functions
+function graphToTree(graph) {
+  var items = [];
+  graph.getEdges().forEach(function(edge) {
+    var p1 = graph.getNodeById(edge.nodes[0]).data.pos,
+        p2 = graph.getNodeById(edge.nodes[1]).data.pos;
+
+    items.push([
+      Math.min(p1[0], p2[0]), Math.min(p1[1], p2[1]),
+      Math.max(p1[0], p2[0]), Math.max(p1[1], p2[1]),
+      edge.id,
+    ]);
+  });
+
+  var tree = rbush();
+  tree.load(items);
+  return tree;
+};
+
+function _extend(obj, otherObj) {
+  if(!obj) { return obj; }
+  for(var i in otherObj) { obj[i] = otherObj[i]; }
+  return obj;
+};
+
+return RealtimeTrafficData;
+})();
